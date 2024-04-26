c9756b03a133f203d006143c98e2b2d2bc4a981d
Manahil rasheed <manahilrasheed507@gmail.com>
2024-04-26 15:16:01 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answers: {
+    sentence: string
+} = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
+])
+
+const word = answers.sentence.trim().split(" ")
+
+// print the array of words to console
+
+console.log(word)
+
+
+// print the word count of the sentence to  the console
+console.log(`Your sentence word count is ${word.length}`);
